4e299f0bf29be9ad200dd62f787b01dfc7473a73
lucia biroli <biroli.lucia@itiscannizzaro.edu.it>
2025-01-19 22:33:21 +0000

    finale

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -93,9 +93,9 @@ public class Main {
      */
     public static boolean isVocale(char carattere) {
         boolean risposta=false;
-        switch (carattere)) {
+        switch (carattere) {
             case 'a','A','e','E','i','I','o','O','u','U':
-                risposta=True;
+                risposta=true;
                 break;
         }
         return risposta;
@@ -112,17 +112,17 @@ public class Main {
     public static String tipoTriangolo(float lato1, float lato2, float lato3) {
         String risposta="equilatero,isoscele,scaleno";
         String[] r=risposta.split(",");
-        float diferenza=0.00001;
-        if(lato1-lato2<differenza&&lato2-lato3<differenza){
-            System.out.println("il triangolo è equilatero");
+        double diferenza=0.0001;
+        if(lato1-lato2<diferenza&&lato2-lato3<diferenza){
+            System.out.print("il triangolo è ");
             return r[0];
         }
-        else if(lato1-lato2<differenza||lato2-lato3<differenza||lato3-lato1<differenza){
-            System.out.println("il triangolo è isoscele");
+        else if(lato1-lato2<diferenza||lato2-lato3<diferenza||lato3-lato1<diferenza){
+            System.out.print("il triangolo è ");
             return r[1];
         }
         else{
-            System.out.println("il triangolo è scaleno");
+            System.out.print("il triangolo è ");
             return r[2];
         }
         
@@ -139,7 +139,7 @@ public class Main {
         String risposta="lunedì,martedì,mercoledì,giovedì,venerdì,sabato,domenica";
         String[] giornoSett=risposta.split(",");
         int numeroGiorno= 7%giorno;   //dividendo per sette il resto darà il giorno della settimana(es. resto 3 =mercoledì)
-        return giornoSett[numeroGiorno];
+        return giornoSett[numeroGiorno-1];
     }
 
     /**
@@ -153,8 +153,8 @@ public class Main {
      */
     public static String ordineDecrescente(int fine, int inizio) {
         String risposta="";
-        for(int i=fine; i>inizio;i--){ 
-            risposta=(risposta+","+i);
+        for(int i=fine; i>inizio;i--){  
+            risposta=(risposta+i+",");
 
         }
         
@@ -178,7 +178,7 @@ public class Main {
         while( spostamento<muro){
             spostamento+=sale;
             risposta++;
-            if(spostamento==numro){
+            if(spostamento>=muro){
                 return risposta;
             }
             spostamento-=scende;
@@ -190,7 +190,7 @@ public class Main {
      * ESERCIZIO 8
      * In un diagramma cartesiano, dati i vertici P1 P2 e P3 di un rettangolo con i lati paralleli/perpendicolari agli assi
      * calcolare le coordinate del punto P4 e stamparlo come stringa separando l'ascissa e l'ordinata con una virgola
-     * es. P1(0,0), P2(0,1), P3(1,0) => coordinatePunto(0,0,0,1,1,0) => 1,1
+     * es. P1(0,0), P2(0,2), P3(1,0) => coordinatePunto(0,0,0,2,1,0) => 1,2
      * @param x1 ascissa P1
      * @param y1 ordinata P1
      * @param x2 ascissa P2
@@ -202,6 +202,31 @@ public class Main {
 
     public static String coordinatePunto(int x1, int y1, int x2, int y2,int x3,int y3) {
         String risposta="";
+        if(x1!=x2&&x1!=x3);{
+            if(y1==y2){
+                risposta=(x1+","+y3);
+            }
+            else{
+                risposta=(x1+","+y2);
+            }
+        }
+        if(x2!=x1&&x2!=x3);{
+            if(y2==y1){
+                risposta=(x2+","+y1);
+            }
+            else{
+                risposta=(x2+","+y3);
+            }
+        }
+        if(x3!=x2&&x3!=x1);{
+            if(y3==y2){
+                risposta=(x3+","+y1);
+            }
+            else{
+                risposta=(x3+","+y2);
+            }
+        }
+        
         
         return risposta;
     }
